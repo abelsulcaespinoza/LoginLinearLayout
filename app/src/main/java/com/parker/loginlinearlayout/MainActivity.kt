567c6723b14533b9fcbd54841fca50f6aff595c0
Abel Sulca Espinoza <abelsulcaespinoza@gmail.com>
2025-12-30 03:32:35 -0500

    update Buttom Sign In

--- app/src/main/java/com/parker/loginlinearlayout/MainActivity.kt
+++ app/src/main/java/com/parker/loginlinearlayout/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.parker.loginlinearlayout
 
 import android.os.Bundle
-import android.widget.TextView
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var btnSignIn: Button
+    lateinit var etEmail: TextInputEditText
+    lateinit var etPassword: TextInputEditText
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,6 +24,24 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        btnSignIn = findViewById(R.id.btnSignIn)
+        etEmail = findViewById(R.id.etEmail)
+        etPassword = findViewById(R.id.etPassword)
+
+
+
+        btnSignIn.setOnClickListener {
+            val email = etEmail.text.toString()
+            val password = etPassword.text.toString()
+
+            if (email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "Email or password is empty", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show()
+            }
+
         }
 
     }
